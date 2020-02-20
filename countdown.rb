18f65c4c23a2d6fb54a70b8d64b809ff0f90fba1
Alexia Adediran <alexiaadediran30@gmail.com>
2020-02-20 04:01:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,19 +1,4 @@
 #write your code here
-def countdown(number, seconds)
-  while number > 0
-  	    countdown_with_sleep(seconds)
-  	    puts "#{number} SECOND(s)!"
-    number -= 1
-  end
-  puts "HAPPY NEW YEAR!"
-end
 
-
-def countdown_with_sleep(seconds)
-  sleep seconds
-end
-
-
-countdown(10, 1)
 
 
